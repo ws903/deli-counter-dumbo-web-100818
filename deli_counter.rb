d28cd193b6dfada511aecb165e85bef5fe63930a
David Sim <wsim903@gmail.com>
2018-09-26 20:02:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,9 @@ def line(arr)
   if arr.size() == 0:
     puts "The line is currently empty."
   else
+    out = "The line is currently:"
     for i in 0...arr.size() do
-      
+      out += " #{i+1}. #{arr[i}"
   
   
 end
